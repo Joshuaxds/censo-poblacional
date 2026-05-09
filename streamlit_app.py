1a356c7bb95ad6b36dc01762057f238bb5fab01b
Joshuaxds <joshuamatizdelgado@gmail.com>
2026-05-09 07:01:03 +0000

    Aplicación final de nacimientos

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,42 @@
 import streamlit as st
-import plotly.express as px
 import pandas as pd 
 from openpyxl import load_workbook
 
-st.title("🎈 My new app")
+#Configuración de la página
+st.set_page_config(
+    page_title="Nacimientos en Colombia DANE",
+    page_icon="📈",
+    layout="wide")
+
+#Titulo y descripción de la pagina
+st.title("Nacimientos en Colombia (2014-2025) ")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+    "Este diagrama de líneas muestra la evolución de los nacimientos en Colombia durante los últimos 10 años:")
+st.divider()
+
+#Ruta del archivo Excel
+archivo = "datos/nacimientos_colombia_2014_2025.xlsx"
+
+# Cargar el archivo Excel
+wb = load_workbook(archivo, data_only=True)
+ws = wb.active
+
+# Crear una lista para almacenar los datos y separar los encabezados de las filas
+datos = list(ws.values)
+encabezados = datos[0]  # Primera fila como encabezados
+filas = datos[1:]  # Resto de las filas como datos
+
+# Crear un DataFrame de pandas con los datos
+df = pd.DataFrame(filas, columns=encabezados)
+df = df[["Año", "Número de Nacimientos"]]
+
+# Asegurar que sean números
+df["Año"] = pd.to_numeric(df["Año"], errors="coerce")
+df["Número de Nacimientos"] = pd.to_numeric(df["Número de Nacimientos"], errors="coerce")
+
+#Gráfico de líneas con Streamlit
+st.line_chart(df, x="Año", y="Número de Nacimientos", width="stretch")
+st.divider()
+
+# Pie de página
+st.caption("Fuente de datos: Departamento Administrativo Nacional de Estadística (DANE)")
